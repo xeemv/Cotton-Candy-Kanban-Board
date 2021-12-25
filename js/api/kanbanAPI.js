28eb191966cd31bedb6ece36ac2b7805c8f08d1e
xeemv <xeeyang08@gmail.com>
2021-12-25 09:49:16 -0600

    updated and fixed flex on css

--- js/api/kanbanAPI.js
+++ js/api/kanbanAPI.js
@@ -1,8 +1,20 @@
 /* 
-- This js file will the local storage components
+- This js file will contain all the local storage components
 - will contain functions to interact w/ the local storage
 */
 
 export default class kanbanAPI{
 
 }
+/* 
+- code line 6 will allow us to import the class into the other js files
+- line 6 will contain a bunch of static methods
+*/
+
+function read(){
+
+}
+/* 
+- This js file will the local storage components
+- will contain functions to interact w/ the local storage
+*/
