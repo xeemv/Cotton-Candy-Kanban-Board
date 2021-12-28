7ba85cbbf3c57170f79a033c4c9747cee9652144
xeemv <xeeyang08@gmail.com>
2021-12-28 00:04:33 -0600

    working on onBlur

--- js/view/Item.js
+++ js/view/Item.js
@@ -8,6 +8,17 @@ export default class Item{
 		this.elements.root.dataset.id = id;
 		this.elements.input.textContent = content;
 		this.content = content;
+
+
+        const onBlur = () => {
+            const newContent = this.elements.input.textContent.trim();
+
+            console.log(this.content);
+            console.log(newContent);
+        };
+
+
+        this.elements.input.addEventListener("blur", onBlur);
     }
 
     static createRoot(){
@@ -36,4 +47,8 @@ export default class Item{
 - code line 5:
     - this.elements.input = this.elements.root.querySelector(".kanban__item-input"); -->
         - is the kanban item input to get a reference to that particular html element
+
+- code line 13:
+    - const onBlur = () => { ---->
+        - this will give the user the ability to update the content of a single item
 */
